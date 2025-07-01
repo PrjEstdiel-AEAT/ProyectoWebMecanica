072de080ac4f68478e6bbd32f6f7ac717089b3be
ErickATE1306 <arnoldantezanatorres@gmail.com>
2025-07-01 15:17:47 -0500

    correcciones del header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { NuestrosServicios } from './paginas/nuestros-servicios/nuestros-servicios';
+import { Nosotros } from './paginas/nosotros/nosotros';
+import { Contacto } from './paginas/contacto/contacto';
+import { Login } from './paginas/login/login';
+import { Registro } from './paginas/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: Inicio
+    }
+    ,
+    {
+        path:'nuestros-servicios',
+        component: NuestrosServicios
+    }
+    ,
+    {
+        path: 'nosotros',
+        component: Nosotros
+    }
+    ,
+    {
+        path: 'contacto',
+        component: Contacto
+    }
+    ,
+    {
+        path: 'login',
+        component: Login
+    }
+    ,
+    {
+        path: 'registro',
+        component: Registro
+    }
+    ,
+    {
+        path: '**',
+        redirectTo: ''  
+    }
+];
--- src/app/paginas/header/header.ts
+++ src/app/paginas/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
